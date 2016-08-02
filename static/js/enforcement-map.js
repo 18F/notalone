47034268fbe944108f65ff72bf30f5ac9622c5f3
Will Slack <wslack@users.noreply.github.com>
2016-08-02 19:46:54 -0400

    edit use of MapQuest to OSM in Data Tab

--- static/js/enforcement-map.js
+++ static/js/enforcement-map.js
@@ -10,8 +10,8 @@ $(document).ready(function() {
     var map = L.map('map', {
       scrollWheelZoom: false,
     }).setView([39.8282, -98.5795], 4);
-    L.tileLayer('https://otile{s}-s.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.jpg', {
-      attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a>',
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       maxZoom: 18,
       subdomains: [1,2,3,4]
     }).addTo(map);
