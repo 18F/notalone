ccbc40c841d6067d1ad361661e0838f262f1d76f
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2016-08-02 20:08:57 -0400

    Use leaflet default subdomains for tile server

--- static/js/service-locator.js
+++ static/js/service-locator.js
@@ -16,10 +16,9 @@ $(document).ready(function () {
   var map = L.map('map', {
     scrollWheelZoom: false
   }).setView([39.8282, -98.5795], 4);
-  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    maxZoom: 18,
-    subdomains: [1, 2, 3, 4]
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    maxZoom: 18
   }).addTo(map);
 
   $('#service-locator-form').on('submit', function(event){
